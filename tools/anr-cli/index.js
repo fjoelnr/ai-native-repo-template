a783f5594980498e88b2b6b5bd9c8c314db75a35
fjoelnir <thrudhamar@web.de>
2026-03-07 13:24:21 +0100

    Implement anr init command

--- tools/anr-cli/index.js
+++ tools/anr-cli/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log("ANR CLI - command not implemented yet")
+const { runInit } = require("./init")
+
+const command = process.argv[2]
+
+if (command === "init") {
+  runInit()
+} else if (command === "validate") {
+  console.log("validate command not implemented yet")
+  process.exitCode = 1
+} else {
+  console.log("Usage: anr <init|validate>")
+  process.exitCode = 1
+}
