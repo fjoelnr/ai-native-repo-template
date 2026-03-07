af0236640d612cdaeb01c9ea12db5eb3ef16e2a9
fjoelnir <thrudhamar@web.de>
2026-03-07 13:24:44 +0100

    Implement anr validate command

--- tools/anr-cli/index.js
+++ tools/anr-cli/index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const { runInit } = require("./init")
+const { runValidate } = require("./validate")
 
 const command = process.argv[2]
 
 if (command === "init") {
   runInit()
 } else if (command === "validate") {
-  console.log("validate command not implemented yet")
-  process.exitCode = 1
+  runValidate()
 } else {
   console.log("Usage: anr <init|validate>")
   process.exitCode = 1
